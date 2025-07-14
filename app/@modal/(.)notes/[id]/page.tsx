029304bf1b9e6c103ca946c001f3ebdcb663dfeb
YeVheniia <didenko11121988@ukr.net>
2025-07-14 17:36:35 +0300

    fix: add generateMetadata to modal page.tsx

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,8 +1,23 @@
-import { fetchNoteById } from "@/lib/api";
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 import NotePreview from "./NotePreview.client";
+import { fetchNoteById } from "@/lib/api";
+import { Metadata } from "next";
 
-export default async function Page({ params }: { params: { id: string } }) {
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  return {
+    title: `Note ${params.id} | NoteHub`,
+  };
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
   const id = Number(params.id);
 
   const queryClient = new QueryClient();
